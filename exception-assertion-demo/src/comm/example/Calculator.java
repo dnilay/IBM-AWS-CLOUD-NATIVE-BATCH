cf103fb8461eb65658b790d24c7f17855d513e5e
Nilay <54245048+dnilay@users.noreply.github.com>
2021-03-05 12:16:03 +0530

    Add files via upload

---
+++ exception-assertion-demo/src/comm/example/Calculator.java
@@ -0,0 +1,18 @@
+package comm.example;
+
+public class Calculator {
+    private int firstNumber;
+    private int secondNumber;
+
+    public Calculator(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public int devide()
+    {
+
+        return firstNumber/secondNumber;
+    }
+
+}
