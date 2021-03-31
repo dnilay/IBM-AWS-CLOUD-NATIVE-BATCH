120edce878e8543aac1fd929e8181fc8f5aa13ba
Nilay <54245048+dnilay@users.noreply.github.com>
2021-03-31 09:37:08 +0530

    Add files via upload

--- customer-service/src/main/java/com/example/demo/controller/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,23 @@ import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@RequestMapping("/api")
+
 public class CustomerController {
 	
 	private CustomerService customerService;
 
+	private Environment environment;
 	@Autowired
-	public CustomerController(CustomerService customerService) {
+	public CustomerController(CustomerService customerService, Environment environment) {
+		super();
 		this.customerService = customerService;
+		this.environment = environment;
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> getStatus()
+	{
+		return ResponseEntity.ok("customer service is up at port number: "+environment.getProperty("local.server.port"));
 	}
 	@PostMapping(path = "/customers")
 	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer)
