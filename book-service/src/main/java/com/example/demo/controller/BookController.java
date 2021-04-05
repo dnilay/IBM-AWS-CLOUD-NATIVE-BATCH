618660a13296ea34dafadda1bf01dd106564e2ed
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-05 09:49:14 +0530

    Add files via upload

---
+++ book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+@RestController
+public class BookController {
+
+	private BookService bookService;
+
+	@Autowired
+	public BookController(BookService bookService) {
+		super();
+		this.bookService = bookService;
+	}
+
+	@GetMapping("/authors/{authorName}")
+	public ResponseEntity<List<Book>> findBooksByAuthorName(@PathVariable("authorName") String authorName)
+	{
+		return ResponseEntity.ok(bookService.findByAuthorName(authorName));
+	}
+
+}
