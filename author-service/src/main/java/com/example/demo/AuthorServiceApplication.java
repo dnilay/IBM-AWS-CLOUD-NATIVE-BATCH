30c3adf6b5b490ca110932a080f618739c325dc9
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-05 17:24:20 +0530

    Add files via upload

--- author-service/src/main/java/com/example/demo/AuthorServiceApplication.java
+++ author-service/src/main/java/com/example/demo/AuthorServiceApplication.java
@@ -6,14 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 import com.example.demo.dao.AuthorDao;
 import com.example.demo.model.Author;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableSwagger2
 public class AuthorServiceApplication implements CommandLineRunner{
 
 	private AuthorDao authorDao;
@@ -35,5 +42,11 @@ public class AuthorServiceApplication implements CommandLineRunner{
 		authorDao.saveAndFlush(new Author(3,"auth-3"));
 		authorDao.saveAndFlush(new Author(4,"auth-4"));
 	}
+	
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
+	   }
 
 }
