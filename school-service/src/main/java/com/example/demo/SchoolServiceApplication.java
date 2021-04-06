28e1c5a94a914d10122e6d09d497d199351622e4
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-06 14:21:08 +0530

    Add files via upload

---
+++ school-service/src/main/java/com/example/demo/SchoolServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+public class SchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServiceApplication.class, args);
+	}
+
+}
