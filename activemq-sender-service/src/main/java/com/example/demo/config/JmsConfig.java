6d18a4c3fdcedd1c05ceeabf0919aa6c565c2fd8
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-07 17:26:29 +0530

    Add files via upload

--- activemq-sender-service/src/main/java/com/example/demo/config/JmsConfig.java
+++ activemq-sender-service/src/main/java/com/example/demo/config/JmsConfig.java
@@ -4,6 +4,7 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.Queue;
 
@@ -11,8 +12,11 @@ import javax.jms.Queue;
 @EnableJms
 public class JmsConfig {
 
-    @Bean
-    public Queue queue(){
-        return new ActiveMQQueue("customer-queue");
-    }
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("test-queue");
+	}
+
+
+
 }
