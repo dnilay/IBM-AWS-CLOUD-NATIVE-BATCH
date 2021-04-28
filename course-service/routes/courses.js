bee5de3c994d0f25a5cd76fd5b1732565c201a59
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-28 14:19:23 +0530

    Add files via upload

--- course-service/routes/courses.js
+++ course-service/routes/courses.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const Joi = require("joi");
 const router = express.Router();
-
 const courses = [
   { id: 1, name: 'course1' },  
   { id: 2, name: 'course2' },  
@@ -11,5 +11,26 @@ router.get('/', (req, res) => {
   res.send(courses);
 });
 
+router.post('/',(request,response)=>{
+  const { error }= validateCourse(request.body);
+  if(error)
+  {
+     return response.status(400).send(error.details[0].message);
+  }
+  const course = {
+    id: courses.length + 1,
+    name: request.body.name
+  };
+  courses.push(course);
+  response.send(course);
+});
 
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+
+}
 module.exports = router; 
