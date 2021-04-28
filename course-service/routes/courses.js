bc091686e1793d134cbbe486ca038f9818211dd8
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-28 15:23:32 +0530

    Add files via upload

--- course-service/routes/courses.js
+++ course-service/routes/courses.js
@@ -24,7 +24,34 @@ router.post('/',(request,response)=>{
   courses.push(course);
   response.send(course);
 });
+router.get('/:id',function (request,response){
+  const course=courses.find(c=>c.id===parseInt(request.params.id));
+  if(!course)
+  {
+    response.status(400).send('course not found with the given id: ');
+  }
+  response.send(course);
+});
+router.put('/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found.');
 
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+router.delete('/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
 function validateCourse(course) {
   const schema = {
     name: Joi.string().min(3).required()
