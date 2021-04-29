98329fa0567514a7729374f15535309aaf947c3d
Nilay <54245048+dnilay@users.noreply.github.com>
2021-04-29 09:47:05 +0530

    Add files via upload

---
+++ mongo-express-integration/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const customers = require('./routes/customer');
+const express = require('express');
+const app = express();
+mongoose.connect('mongodb://localhost/vidly',{ useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/customers', customers);
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
